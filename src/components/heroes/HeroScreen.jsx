531fd339dfacaa9e3934f5c69e02d57f27d44a76
sensui <sensuijunior@hotmail.com>
2020-09-19 02:30:52 -0300

    hero detail

--- src/components/heroes/HeroScreen.jsx
+++ src/components/heroes/HeroScreen.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Redirect, useParams } from 'react-router-dom'
+import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
+
+	const { heroeId } = useParams();
+
+	const hero = getHeroById(heroeId);
+
+	if (!hero) {
+		return <Redirect to="/" />;
+	}
+
+	const {
+		superhero,
+		publisher,
+		alter_ego,
+		first_appearance,
+		characters
+	} = hero;
+
 	return (
 		<div>
-
+			<h1>Hero Screen</h1>
 		</div>
 	)
 }
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -4,20 +4,20 @@ import {
 	Switch,
 	Route
 } from "react-router-dom";
+
 import { LoginScreen } from '../components/login/LoginScreen';
-import { MarvelScreen } from '../components/marvel/MarvelScreen';
-import { Navbar } from '../components/ui/NavBar';
+import { DashboardRoutes } from './DashboardRoutes';
 
 export const AppRouter = () => {
 	return (
 		<Router>
 			<div>
-				<Navbar/>
+
 				<Switch>
 					<Route exact path="/login" component={LoginScreen} />
 
 
-					<Route exact path="/" component={MarvelScreen} />
+					<Route path="/" component={ DashboardRoutes } />
 
 
 
