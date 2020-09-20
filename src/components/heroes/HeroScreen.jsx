801471d8f9bcd52bcf5f34e12e21d9be76323790
sensui <sensuijunior@hotmail.com>
2020-09-20 01:42:17 -0300

    starting searchScreen

--- src/components/heroes/HeroScreen.jsx
+++ src/components/heroes/HeroScreen.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import { Redirect, useParams } from 'react-router-dom'
+import React, { useMemo } from 'react';
+import { Redirect, useParams } from 'react-router-dom';
 import { getHeroById } from '../../selectors/getHeroById';
 
-export const HeroScreen = () => {
+export const HeroScreen = ({ history }) => {
 
 	const { heroeId } = useParams();
-
-	const hero = getHeroById(heroeId);
+	const hero = useMemo(() => getHeroById(heroeId), [heroeId])
 
 	if (!hero) {
 		return <Redirect to="/" />;
@@ -20,9 +19,44 @@ export const HeroScreen = () => {
 		characters
 	} = hero;
 
+	const handleReturn = () => {
+
+		if (history.lenght <= 2) {
+			history.push('/');
+		} else {
+			history.goBack();
+		}
+
+	}
+
+
 	return (
-		<div>
-			<h1>Hero Screen</h1>
+		<div className="row mt-5">
+			<div className="col-4">
+				<img
+					src={`../assets/heroes/${heroeId}.jpg`}
+					alt={superhero}
+					className="img-thumbnail animate__animated animate__fadeInLeft"
+				/>
+			</div>
+			<div className="col-8 animate__animated animate__fadeIn">
+				<h3>{superhero}</h3>
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item"> <b>Aleter ego: </b> {alter_ego} </li>
+					<li className="list-group-item"> <b>Publisher: </b> {publisher} </li>
+					<li className="list-group-item"> <b>First appearance: </b> {first_appearance} </li>
+				</ul>
+				<h5>Characters</h5>
+				<p> {characters} </p>
+
+				<button
+					className="btn btn-outline-info"
+					onClick={handleReturn}
+				>
+					Return
+				</button>
+			</div>
+
 		</div>
 	)
 }
